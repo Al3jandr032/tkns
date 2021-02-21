248aa7865171db1a21b7acc1293cea5ee9794082
Alex <pedrolop15@hotmail.com>
2021-02-21 01:39:58 -0600

    first commit

---
+++ test/token.py
@@ -0,0 +1,13 @@
+import tkns
+
+token = tkns.token()
+print(token)
+print(type(token))
+
+token = tkns.tokenRandom()
+print(token)
+print(type(token))
+
+randomString = tkns.randomLetters()
+print(randomString)
+print(type(token))
